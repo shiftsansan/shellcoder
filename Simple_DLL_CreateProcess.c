3be1347b77d89bd03bd1d10553c16c8e752160af
shiftsansan <104987706+shiftsansan@users.noreply.github.com>
2022-08-03 23:26:18 -0700

    Create Simple_DLL_CreateProcess.c

---
+++ Simple_DLL_CreateProcess.c
@@ -0,0 +1,56 @@
+//Add .def
+//LIBRARY "dllmain"
+//EXPORTS
+//wow
+
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <stdlib.h>
+#include <iostream>
+
+
+int wow() {
+	MessageBox(0, TEXT("OK1"), TEXT("OK"), MB_OK);
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	if (!
+		CreateProcess
+		(
+			TEXT("C:\\Users\\Jupi\\source\\repos\\Dll2\\x64\\Debug\\a.bat"),
+			NULL, NULL, NULL, FALSE,
+			CREATE_NEW_CONSOLE,
+			NULL, NULL,
+			&si,
+			&pi
+		)
+		)
+		std::cout << "Unable to execute.";
+
+	return TRUE;
+}
+
+BOOL APIENTRY dllmain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+
+
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        wow();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
